dc85e455bc6280821ef6f47cc47016b5d851aa6a
Fábio <fabiopv1994@gmail.com>
2020-08-13 10:19:35 -0300

    implementado lib para teste

--- lib/Screens/OpenOccurrenceScreen/OpenOccurrenceScreen.dart
+++ lib/Screens/OpenOccurrenceScreen/OpenOccurrenceScreen.dart
@@ -1,8 +1,43 @@
+import 'package:camera/camera.dart';
+import 'package:fiscaliza_ja/main.dart';
 import 'package:flutter/material.dart';
 
-class OpenOccurrenceScreen extends StatelessWidget {
+class OpenOccurrenceScreen extends StatefulWidget {
+  @override
+  _OpenOccurrenceScreenState createState() => _OpenOccurrenceScreenState();
+}
+
+class _OpenOccurrenceScreenState extends State<OpenOccurrenceScreen> {
+  CameraController controller;
+
+  @override
+  void initState() {
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((value) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+
+    return AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: CameraPreview(controller),
+    );
     return Container();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
-import 'package:fiscaliza_ja/Models/Occurrence.dart';
 import 'package:fiscaliza_ja/Screens/HomeScreen/HomeScreen.dart';
 import 'package:fiscaliza_ja/Screens/OccurrenceDetailScreen/OccurrenceDetailScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:camera/camera.dart';
+
+List<CameraDescription> cameras;
 
 void main() async {
   await DotEnv().load('.env');
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(Main());
 }
 
