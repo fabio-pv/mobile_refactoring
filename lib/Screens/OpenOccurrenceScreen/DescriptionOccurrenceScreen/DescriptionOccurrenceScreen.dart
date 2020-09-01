860fbc29ad8dbd83acd708af986e440e02365bc1
Fábio <fabiopv1994@gmail.com>
2020-09-01 15:30:26 -0300

    criando tela de grupos

--- lib/Screens/OpenOccurrenceScreen/DescriptionOccurrenceScreen/DescriptionOccurrenceScreen.dart
+++ lib/Screens/OpenOccurrenceScreen/DescriptionOccurrenceScreen/DescriptionOccurrenceScreen.dart
@@ -1,5 +1,8 @@
 import 'package:fiscaliza_ja/Controllers/GroupController.dart';
+import 'package:fiscaliza_ja/Models/Group.dart';
+import 'package:fiscaliza_ja/Patterns/GenericPattern.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class DescriptionOccurrenceScreen extends StatefulWidget {
   @override
@@ -15,6 +18,8 @@ class _DescriptionOccurrenceScreenState
 
   _DescriptionOccurrenceScreenState({@required this.groupController});
 
+  List<Group> _listGroup = [];
+
   @override
   void initState() {
     this.load();
@@ -24,7 +29,10 @@ class _DescriptionOccurrenceScreenState
   Future<void> load() async {
     try {
       final list = await this.groupController.getList();
-      print(list);
+
+      setState(() {
+        this._listGroup = list;
+      });
     } catch (e) {
       print('Error');
       print(e);
@@ -33,10 +41,64 @@ class _DescriptionOccurrenceScreenState
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Center'),
-      ),
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            height: 250,
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+        ListView.separated(
+          padding: EdgeInsets.only(
+            top: 200,
+          ),
+          itemCount: this._listGroup.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              child: Column(
+                children: [
+                  Container(
+                    height: 100,
+                    child: ListView(
+                      padding: EdgeInsets.only(left: 60,),
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 100,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          height: 100,
+                          width: 100,
+                          color: Colors.blue,
+                        ),
+                        Container(
+                          height: 100,
+                          width: 100,
+                          color: Colors.teal,
+                        ),
+                        Container(
+                          height: 100,
+                          width: 100,
+                          color: Colors.cyan,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) {
+            return SizedBox(
+              height: 50,
+            );
+          },
+        ),
+      ],
     );
   }
 }
